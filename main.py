ab3cc425f957092d852fcd87fdcaf51f8b8c085c
Peter Fiee <peterfiee@icloud.com>
2023-07-13 01:16:29 -0400

    general rework

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import pandas as pd
-import streamlit as st
-
-from recommend import recommend_books_to_readers, recommend_readers_for_book
 from itertools import permutations
 from pathlib import Path
+
+import pandas as pd
+import streamlit as st
+from recommend import recommend_books_to_readers, recommend_readers_for_book
 
 
 def create_pairs(col) -> pd.DataFrame:
@@ -16,9 +16,7 @@ def create_pairs(col) -> pd.DataFrame:
         pd.DataFrame: pandas dataframe
     """
 
-    pairs = pd.DataFrame(list(permutations(col, 2)), columns=["book_a", "book_b"])
-    return pairs
-
+    return pd.DataFrame(list(permutations(col, 2)), columns=["book_a", "book_b"])
 
 def return_book_pairs(library : pd.DataFrame, book_title : "str") -> pd.DataFrame:
     """Take a dataframe and book title as inputs, and return the top 10 books most frequently paired with the book
@@ -46,32 +44,53 @@ def main() -> None:
 
     df = pd.read_parquet(file_path)
 
-    st.title("Book Recommandation Web App")
-    st.text("Welcome to our web app, designed to provide personalized book recommendations.")
+    user_list = df['userId'].unique().tolist()
+    book_list = df['title'].unique().tolist()
+    isbn_list = df['isbn13'].unique().tolist()
 
-    role = st.radio("Choose your role", ("Registered User", "Unregistered User", "Librarian"))
+    st.title("Book Recommandation Web App")
+    st.text(
+        """Welcome to the Book Recommender. Please choose one of the following roles:
+        1. Unregistered User: You have no profile in the system, and will only get non-personalized recommendations
+        2. Registered User: You have an active profile, and will get recommendations based on your past reading habits
+        3. Librarian: Recommend a book to a group of select readers who might be interested in it.
+        """)
+
+    role = st.radio("Choose your role:", ("Unregistered User", "Registered User", "Librarian"))
 
     if role == "Unregistered User":
-        st.header("Welcome, Stranger! ")
-        bookname = st.text_input(label = "Please enter a book name you like and we can start book recommandation from there" )
-        st.subheader("Non-Personalized Recommendations")
+        st.header("Welcome, Stranger!")
+        bookname = st.selectbox(label="Enter Book title to get non-personalized recommendations", options=book_list)
+        st.subheader("Recommendations")
         st.text(f"Top ten books frequently read with `{bookname}`")
         book_rec = return_book_pairs(df, book_title = bookname)
     elif role == "Registered User":
-        st.header("Welcome back!")
-        user_id = st.number_input(label="Please enter your User ID, and we can recommend some books")
-        st.subheader("The following books might be of interest")
+        user_id = _create_menu(
+            "Welcome back!",
+            "Please enter your User ID, and we can recommend some books",
+            "The following books might be of interest",
+            options=user_list
+        )
         book_rec = recommend_books_to_readers(user_id=user_id)
     else:
-        st.header("Hello, Master Librarian")
-        isbn = st.number_input(label="Please enter an ISBN to recommend a book to new readers")
-        st.subheader("The following 5 users might be interested in that book")
+        isbn = _create_menu(
+            "Master Librarian",
+            "Please enter an ISBN to recommend a book to new readers",
+            "The following 5 users might be interested in that book",
+            options=isbn_list
+        )
         book_rec = recommend_readers_for_book(isbn=isbn)
 
-    startAnalysis = st.sidebar.button("Get Recommendations")
-
-    if startAnalysis:
+    if startAnalysis := st.sidebar.button("Get Recommendations"):
         st.write(book_rec)
+
+
+# TODO #1 Rename this here and in `main`
+def _create_menu(arg0, label, options, arg2):
+    st.header(arg0)
+    result = st.selectbox(label=label, options=options)
+    st.subheader(arg2)
+    return result
 
 
 if __name__ == "__main__":
